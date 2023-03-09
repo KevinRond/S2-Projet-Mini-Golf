73c39f2f2997179801fbb4df30a2057a82464b0a
KevinRond <ronk2602@usherbrooke.ca>
2023-03-09 14:08:12 -0500

    Update Classe Coup
    Calcul maintenant acceleration ainsi que les vitesses initiales en x, y du coup

--- Mini-Golf/Coup.h
+++ Mini-Golf/Coup.h
@@ -2,23 +2,31 @@
 #define COUP_H
 #include <iostream>
 #include <string>
+#include <cmath>
+#define K 10
 
 class Coup
 {
 private:
-	double directionX, directionY;	//Vecteur de direction unitaire X[0],Y[1] ()
+	double direction;	
 	double amplitude;				//Amplitude de force (lecture de l'accelerometre)
+	double vitesseX, vitesseY;
+	double acceleration;
 
 public:
 	Coup();
 	~Coup();
 
 	//Setter
-	void Direction(double DirX, double DirY);		//Direction choisie + random factor
-	void Amplitude(double module);		//Lecture de l'amplitude
+	void setDirection(double DirX, double DirY);		//Direction choisie + random factor
+	void setAmplitude(double module);		//Lecture de l'amplitude
+	double degToRad(double deg);
+	void calculVitesseX();
+	void calculVitesseY();
+	void calculAcceleration();
 
 	//Getter
-	double GetDirection();	//Lecture de la direction
-	double GetAmplitude();	//Lecture de l'amplitude
+	double getDirection();	//Lecture de la direction
+	double getAmplitude();	//Lecture de l'amplitude
 };
 #endif
