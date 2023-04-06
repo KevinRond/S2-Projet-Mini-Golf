37978b3ebc58043c7ea0d24305991cdc4b8b2f6c
alisow086 <alisow086@gmail.com>
2023-04-06 10:06:50 -0400

    fenetre 1
    Reglement

--- Mini-Golf/Interface.h
+++ Mini-Golf/Interface.h
@@ -8,25 +8,29 @@
 #include <qpushbutton.h>
 //#include <QMediaPlayer>
 #include <QtWidgets/QTextEdit>
+#include <QObject>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QPushButton>
 
 
 
-class Labo1QTApp7 : public QMainWindow
+class Labo1QTApp7 : public QWidget
 {
     Q_OBJECT
 
 public:
     Labo1QTApp7(QWidget* parent = nullptr);
     ~Labo1QTApp7();
-    void afficherMessage();
-
+  
 private:
     //Ui::Labo1QTApp7Class ui;
-    QMenu* fichier;
-    QMenu* edition;
-    QAction* ouvrir;
-    QAction* fermer;
-    QAction* trouver;
-    QPushButton* button;
-    QTextEdit* textEdit;
+    QVBoxLayout* m_layout;
+    QLabel* m_labelLogo;
+    QLabel* m_labelVersion;
+    QLabel* m_labelDescription;
+    QLabel* m_labelCreateur;
+    QPushButton* m_boutonFermer;
+
 };
